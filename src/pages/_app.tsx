b318496dc99a48c7079b1abe34c081fe6382f9c8
yondo123 <46988995+yondo123@users.noreply.github.com>
2022-11-14 01:00:57 +0900

    feat: 레이아웃 제외 컴포넌트 경로 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,11 @@ import GlobalStyle from '@styles/GlobalStyles';
 import '@styles/globals.css';
 import Layout from '@components/Layout';
 import React from 'react';
+import { ExcludeLocation } from '../types';
 
-function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps, ...appProps }: AppProps) {
+  if ([`${ExcludeLocation.Login}`, `${ExcludeLocation.NOTFOUND}`].includes(appProps.router.pathname))
+    return <Component {...pageProps} />;
   return (
     <div>
       <GlobalStyle />
