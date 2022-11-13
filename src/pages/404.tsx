89fd6c3424f9c4f53230f071980b363148db7012
yondo123 <46988995+yondo123@users.noreply.github.com>
2022-11-14 01:01:23 +0900

    add: 로그인 페이지, 404 커스텀 페이지 추가

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,22 @@
 function NotFound() {
-  return <h1>not</h1>;
+  return (
+    <section className="h-screen flex items-center  p-16 bg-gray-900 text-fontSub">
+      <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+        <div className="max-w-md text-center">
+          <h2 className="mb-8 font-extrabold text-9xl ">
+            <span className="sr-only">Error</span>404
+          </h2>
+          <p className="text-2xl mb-8 mt-4 font-semibold md:text-3xl">존재하지 않는 페이지 입니다.</p>
+          <a
+            rel="noopener noreferrer"
+            href="##"
+            className="px-8 py-3 mt-20 font-semibold rounded bg-primary hover:bg-slate-500 text-white">
+            메인 페이지로 이동
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default NotFound;
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,5 +1,28 @@
 function Login() {
-  return <h2>Login</h2>;
+  return (
+    <div className="h-screen bg-white flex flex-col space-y-10 justify-center items-center">
+      <div className="bg-white w-96 shadow-xl rounded p-5">
+        <h1 className="text-3xl font-medium">Sign In</h1>
+        <p className="text-sm">Welcome To PREFACE</p>
+
+        <form className="space-y-5 mt-5">
+          <input
+            type="text"
+            className="w-full h-12 border border-gray-800 rounded px-3 outline-none"
+            placeholder="아이디를 입력하세요."
+          />
+          <div className="w-full flex items-center border border-gray-800 rounded px-3">
+            <input type="password" className="w-4/5 h-12 outline-none" placeholder="비밀번호를 입력하세요." />
+          </div>
+          <button
+            type="button"
+            className="text-center w-full bg-primary hover:bg-slate-500 rounded-md text-white py-3 font-medium">
+            로그인
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
