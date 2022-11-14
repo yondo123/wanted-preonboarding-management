5f2a81e3139bfb3688cca415df0877c71d5d7365
yondo123 <46988995+yondo123@users.noreply.github.com>
2022-11-15 01:37:12 +0900

    test: login 테스트

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,20 +1,47 @@
+import requestLogin from '@utils/httpClient';
+import { useState } from 'react';
+
 function Login() {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleChageEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setEmail(value);
+  };
+
+  const handleChagePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setPassword(value);
+  };
+
+  const handleClick = () => {
+    requestLogin(email, password).then((res) => console.log(res));
+  };
+
   return (
     <div className="h-screen bg-white flex flex-col space-y-10 justify-center items-center">
       <div className="bg-white w-96 shadow-xl rounded p-5">
-        <h1 className="text-3xl font-medium">Sign In</h1>
-        <p className="text-sm">Welcome To PREFACE</p>
+        <h1 className="text-3xl font-medium">로그인</h1>
+        <p className="text-sm bold">Welcome To PREFACE</p>
 
         <form className="space-y-5 mt-5">
           <input
+            onChange={handleChageEmail}
             type="text"
             className="w-full h-12 border border-gray-800 rounded px-3 outline-none"
             placeholder="아이디를 입력하세요."
           />
           <div className="w-full flex items-center border border-gray-800 rounded px-3">
-            <input type="password" className="w-4/5 h-12 outline-none" placeholder="비밀번호를 입력하세요." />
+            <input
+              type="password"
+              className="w-4/5 h-12 outline-none"
+              placeholder="비밀번호를 입력하세요."
+              onChange={handleChagePassword}
+            />
           </div>
           <button
+            onClick={handleClick}
             type="button"
             className="text-center w-full bg-primary hover:bg-slate-500 rounded-md text-white py-3 font-medium">
             로그인
