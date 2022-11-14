89b57404541e278d082a0bf900b91a611a03a60a
yondo123 <46988995+yondo123@users.noreply.github.com>
2022-11-15 01:40:18 +0900

    update: next api 요청 주소 맵핑 설정 변경

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:4000/:path*',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
