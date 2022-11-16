7bff54af08f29d8af2dfff767a6a2f5273abfdc8
yondo123 <46988995+yondo123@users.noreply.github.com>
2022-11-17 02:05:57 +0900

    feat: footer layout

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="p-4 w-full font-semibold text-center text-xs text-primary">
+      Cppyright &copy; December and Compnay Inc.
+    </footer>
+  );
+}
+
+export default Footer;
