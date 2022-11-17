2977c9eaa89561c5f1a017e07d40483b2a23d6ea
yondo123 <46988995+yondo123@users.noreply.github.com>
2022-11-17 11:26:18 +0900

    add: recoil atoms

--- src/recoil/index.ts
+++ src/recoil/index.ts
@@ -1,6 +1,17 @@
 import { atom } from 'recoil';
+import type { Column } from '../types/index';
 
 export const modalState = atom({
   key: 'modalState',
   default: { show: false, message: '' },
+});
+
+export const listColumnState = atom<Column[]>({
+  key: 'listColumnState',
+  default: [],
+});
+
+export const pageState = atom({
+  key: 'pageState',
+  default: 1,
 });
